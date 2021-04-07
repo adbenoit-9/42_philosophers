2c0a73dc12c0481f5b7fc279c1357af5f4ec64f1
Adeline BENOIT <adbenoit@e2r8p8.42.fr>
2021-04-07 18:07:05 +0200

    init

--- philo_three/philo_three.h
+++ philo_three/philo_three.h
@@ -6,11 +6,18 @@
 /*   By: adbenoit <adbenoit@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/06 17:14:07 by adbenoit          #+#    #+#             */
-/*   Updated: 2021/04/06 17:14:17 by adbenoit         ###   ########.fr       */
+/*   Updated: 2021/04/07 14:11:47 by adbenoit         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PHILO_THREE_H
 #define PHILO_THREE_H
+
+# include <pthread.h>
+# include <semaphore.h>
+# include <fcntl.h>
+# include <sys/stat.h>
+# include <stdio.h>
+# include <unistd.h>
 
 #endif
--- philo_two/philo_two.h
+++ philo_two/philo_two.h
@@ -6,11 +6,18 @@
 /*   By: adbenoit <adbenoit@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/06 17:14:20 by adbenoit          #+#    #+#             */
-/*   Updated: 2021/04/06 17:14:28 by adbenoit         ###   ########.fr       */
+/*   Updated: 2021/04/07 14:11:54 by adbenoit         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PHILO_TWO_H
 #define PHILO_TWO_H
+
+# include <pthread.h>
+# include <semaphore.h>
+# include <fcntl.h>
+# include <sys/stat.h>
+# include <stdio.h>
+# include <unistd.h>
 
 #endif
