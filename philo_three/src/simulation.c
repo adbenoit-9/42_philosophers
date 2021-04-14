ee4cf2bb9b05128bd5f9663bbd43c0116a3ba4f0
Adeline Benoit <Adeline@MacBook-Pro.local>
2021-04-14 19:13:27 +0200

    salut

--- philo_three/src/simulation.c
+++ philo_three/src/simulation.c
@@ -6,7 +6,7 @@
 /*   By: adbenoit <adbenoit@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/13 20:00:30 by adbenoit          #+#    #+#             */
-/*   Updated: 2021/04/14 18:01:36 by adbenoit         ###   ########.fr       */
+/*   Updated: 2021/04/14 19:13:05 by adbenoit         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,7 +40,6 @@ static void	routine(t_philo *philo)
 		ft_sleep(philo, i);
 		philo_state(philo, i + 1, THINK);
 	}
-	printf("end\n");
 	exit(0);
 }
 
@@ -64,47 +63,27 @@ int			simulation(void)
 
 	if (gettimeofday(&tv, NULL) == -1)
 		return (printf("Get Time Error.\n"));
-	// g_start_time = tv.tv_sec * 1000 + tv.tv_usec / 1000;
+	g_start_time = tv.tv_sec * 1000 + tv.tv_usec / 1000;
 	i = 0;
-	g_data.philo[i].pid = fork();
-	// g_start_time = tv.tv_sec * 1000 + tv.tv_usec / 1000;
-	waitpid(g_data.philo[i].pid, NULL, 0);
-	if (g_data.philo[i].pid == 0)
+	while (i < g_data.n)
+	{
+		g_data.philo[i].pid = fork();
+		if (g_data.philo[i].pid == -1)
+			return (printf("Fork Error.\n"));
+		waitpid(g_data.philo[i].pid, NULL, 0);
+		if (g_data.philo[i].pid == 0)
 		{
-			// exit(0);
-			// printf("son\n");
-			g_start_time = tv.tv_sec * 1000 + tv.tv_usec / 1000;
+			g_start_time += get_time();
 			routine(&g_data.philo[i]);
 		}
-	// while (i < g_data.n)
-	// {
-	// 	g_data.philo[i].pid = fork();
-	// 	if (g_data.philo[i].pid == -1)
-	// 		return (printf("Fork Error.\n"));
-	// 	if (g_data.philo[i].pid == 0)
-	// 	{
-	// 		// exit(0);
-	// 		printf("son\n");
-	// 		routine(&g_data.philo[i]);
-	// 	}
-	// 	printf("pid = %d\n", g_data.philo[i].pid);
-	// 	ret = waitpid(g_data.philo[i].pid, NULL, 0);
-	// 	printf("ret = %d\n", ret);
-	// 	++i;
-	// }
+		++i;
+	}
 
-	// i = 0;
-	// while (i < g_data.n)
-	// {
-	// 	printf("test\n");
-	// 	// printf("pid = %d\n", g_data.philo[i].pid);
-	// 	ret = waitpid(g_data.philo[i].pid, NULL, 0);
-	// 	printf("ret = %d\n", ret);
-	// 	if (g_data.philo[i].pid == 0)
-	// 		printf("soooon");
-	// 	++i;
-	// }
-	// printf("???\n");
-	// kill_process(i);
+	i = 0;
+	while (i < g_data.n)
+	{
+		// waitpid(g_data.philo[i].pid, NULL, 0);
+		++i;
+	}
 	return (0);
 }
